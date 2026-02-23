59b3057c9e1fe82eaf67601f3aee4dfe93486f88
Alexandr <alpinefuchs@gmail.com>
2026-02-24 02:01:22 +0300

    feat: select Level

--- src/pages/gamePage/GamePage.tsx
+++ src/pages/gamePage/GamePage.tsx
@@ -1,23 +1,40 @@
-import { useEffect, useMemo, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { KeyBoard } from '../../components/keyboard/KeyBoard'
 import { Row } from '../../components/row/Row'
 import type { LetterStatus } from '../../types/LetterStatus';
 import toast from 'react-hot-toast';
 import type { GameStatus } from '../..//types/GameStatus';
+import { api } from '../../app/Api';
+import { useNavigate } from 'react-router-dom';
+import type { Level } from './Level';
+import { Button, Dropdown, type MenuProps } from 'antd';
 
 export function GamePage(){
-    const misteryWord = "APPLE";
+
     const [currRow, setCurrRow] = useState(0);
     const [attemps, setAttemps] = useState<string[]>(["", "", "", "", "", ""]);
     const [currAttempt, setCurrAttempt] = useState("");
-    const [checks, setChecks] = useState<boolean[]>([false, false, false, false, false, false]);
+    const [statuses, setStatuses] = useState<LetterStatus[][]>(
+        Array(6).fill(["unused","unused","unused","unused","unused"])
+    );
+
     const containerRef = useRef<HTMLDivElement>(null);
-    
     const [gameState, setGameState] = useState<GameStatus>("playing");
+    const [level, setLevel] = useState<Level>("B1");
+
+    const items: MenuProps['items'] = ["A1", "A2", "B1", "B2"].map((level) => {
+        return ({
+            key: level,
+            label: level,
+            onClick: () => setLevel(level as Level)
+        });
+    });
 
     useEffect(() => {
         containerRef.current?.focus();
     }, []);
+
+    const navigate = useNavigate();
     const KEYS: Array<string[]> = [
         ["Q","W","E","R","T","Y","U","I","O","P"],
         ["A","S","D","F","G","H","J","K","L"],
@@ -27,155 +44,133 @@ export function GamePage(){
     const defaultKeysStatues = new Map<string, LetterStatus>();
     KEYS.flat().forEach((key) => {
         defaultKeysStatues.set(key, "unused");
-    })
-
-    const statuses = useMemo(() => {
-        const defaultStatus: LetterStatus[] = ["unused", "unused", "unused", "unused", "unused"];
-        return attemps.map((attemp, i) => (
-            checks[i] ? checkWord(attemp) : defaultStatus
-        )) 
-    }, [attemps, checks]);
+    });
 
     const [keysStatuses, setKeysStatuses] = useState<Map<string, LetterStatus>>(defaultKeysStatues);
-    useEffect(() => {
-        if (currRow != 0){
-            setChecks((prev) => {
-                const newChecks = [...prev];
-                newChecks[currRow-1] = true;
-                return newChecks;
-            });
-            setKeysStatuses((prev) => {
-                const newKeysStatuses = new Map(prev);
-                const stats: LetterStatus[] = checkWord(attemps[currRow-1]);
-                stats.forEach((stat,i) => {
-                    const oldStat = newKeysStatuses.get(attemps[currRow-1].charAt(i))!;
-                    switch(oldStat) {
-                        case "unused": 
-                            newKeysStatuses.set(attemps[currRow-1].charAt(i), stat);
-                            console.log();
-                            break;
-                        case "includes":
 
-                            if (stat == "correct"){
-                                newKeysStatuses.set(attemps[currRow-1].charAt(i), stat);
-                            }
-                            break;
-                    }
-                });
-                return newKeysStatuses;
-             })
-        }
-
-        if (misteryWord === attemps[currRow-1]){
-            setGameState("won");
-        } else {
-            if (currRow > 5) {
-                setGameState("lost");
-            }
-        }
-    }, [currRow]);
-
-    useEffect(() => {
-        if (gameState === "won") {
-            toast.success("Good job, boy");
-            setTimeout(() => {
-                newGame();
-            }, 800); 
-        }
-
-        if (gameState === "lost") {
-            toast.error("Game Over! The word was " + misteryWord);
-            setTimeout(() => {
-                newGame();
-            }, 800);
-        }
-    }, [gameState]);
-
-
-    function checkWord(value: string): LetterStatus [] {
-
-        const status: LetterStatus[] = ["unused", "unused", "unused", "unused", "unused"];
-        
-        const letterCounts = new Map<string, number>();
-        misteryWord.split("").forEach((letter) => {
-            letterCounts.set(letter, (letterCounts.get(letter) ?? 0) + 1);
+    async function checkWord(value: string): Promise<LetterStatus[]> {
+        const response = await api.post("/game/daily/check", {
+            attempt: value,
+            level: level
         });
-
-        for (let i = 0; i < 5; i++){
-    
-            if (letterCounts.has(value[i]) && letterCounts.get(value[i])! > 0){
-                
-                if (misteryWord.charAt(i) == value[i]){
-                    status[i] = "correct";
-                } else{
-                    status[i] = "includes";
-                }
-                letterCounts.set(value[i], letterCounts.get(value[i])! -1 );
-            }
-            if (!letterCounts.has(value[i])){
-                status[i] = "wrong";
-            }
-        }
-
-        return status;
-
+        return response.data.statuses;
     }
 
-    function handleClick(letter: string){
+    async function handleClick(letter: string){
+
         switch(letter) {
+
             case "⌫":
                 setCurrAttempt((prev) => prev.slice(0, -1));
                 break;
-        
+
             case "ENTER":
-                
 
                 if (currAttempt.length !== 5) {
                     toast.error("must be 5 letters");
                     break;
                 }
 
-                setCurrRow((prev) => prev + 1);
+                const attemptIndex = currRow; 
+
                 setAttemps((prev) => {
                     const newAttemps = [...prev];
-                    newAttemps[currRow] = currAttempt;
+                    newAttemps[attemptIndex] = currAttempt;
                     return newAttemps;
                 });
 
+                const result = await checkWord(currAttempt);
+
+                setStatuses((prev) => {
+                    const newStatuses = [...prev];
+                    newStatuses[attemptIndex] = result;
+                    return newStatuses;
+                });
+                
+
+
+                setKeysStatuses((prev) => {
+                    const newKeysStatuses = new Map(prev);
+
+                    result.forEach((stat,i) => {
+                        const letter = currAttempt.charAt(i);
+                        const oldStat = newKeysStatuses.get(letter) ?? "unused";
+
+                        if (oldStat === "unused" || 
+                           (oldStat === "includes" && stat === "correct")) {
+                            newKeysStatuses.set(letter, stat);
+                        }
+                    });
+
+                    return newKeysStatuses;
+                });
+
+                if (result.every((status) => status === "correct")) {
+                    toast.success("You win!");
+                    setGameState("won");
+                    return;
+                } else if (currRow === 5) {
+                    const response = await api.get("/game/daily/word", {params: {level: level}});
+                    toast.error("You lost! The word was: " + "" + response.data);
+                    setGameState("lost");
+                    return;
+                }
+
+                setCurrRow((prev) => prev + 1);
                 setCurrAttempt("");
                 break;
 
             default:
-                setCurrAttempt((prev) => prev.length < 5 ? (prev || "") + letter : prev);
+                setCurrAttempt((prev) => prev.length < 5 ? prev + letter : prev);
         }
-        
     }
 
     function newGame(){
         setCurrRow(0);
         setAttemps(["", "", "", "", "", ""]);
-        setChecks([false, false, false, false, false, false]);
+        setStatuses(Array(6).fill(["unused","unused","unused","unused","unused"]));
         setKeysStatuses(defaultKeysStatues);
         setGameState("playing");
         setCurrAttempt("");
     }
 
     return (
-        <>
-            
-            <div ref={containerRef}tabIndex={0} className="game-container"
-    onKeyDown={(e) => {
+        <div
+            ref={containerRef}
+            tabIndex={0}
+            className="game-container"
+            onKeyDown={(e) => {
                 const key = e.key.toUpperCase();
 
                 if (key === "BACKSPACE") handleClick("⌫");
                 else if (key === "ENTER") handleClick("ENTER");
-                else if (/^[A-Z]$/.test(key.toUpperCase())) handleClick(key.toUpperCase());
-            }}>  
-                {Array.from({length: 6}).map((_, i) => (
-                    <Row key={i} value={i == currRow ? currAttempt : attemps[i]} isSelected={i == currRow} status={statuses[i]}/>
-                ))} 
-                <KeyBoard  handleClick={handleClick} KEYS={KEYS} keysStatuses={keysStatuses}/>
-            </div>
-        </>
+                else if (/^[A-Z]$/.test(key)) handleClick(key);
+            }}
+
+            style={{display: "flex", flexDirection: "column", alignItems: "center", gap: "20px"}}
+        >
+
+            <Dropdown menu={{items}} placement='top' arrow = {true}>
+                <Button>{level}</Button>
+            </Dropdown>
+            {Array.from({length: 6}).map((_, i) => (
+                <Row
+                    key={i}
+                    value={i === currRow ? currAttempt : attemps[i]}
+                    isSelected={i === currRow}
+                    status={statuses[i]}  
+                />
+            ))}
+
+            <KeyBoard
+                handleClick={handleClick}
+                KEYS={KEYS}
+                keysStatuses={keysStatuses}
+            />
+
+            {(gameState === "won" || gameState === "lost") && (
+                <button onClick={() => navigate("/")}>Go to Start</button>
+            )}
+        </div>
     )
 }
